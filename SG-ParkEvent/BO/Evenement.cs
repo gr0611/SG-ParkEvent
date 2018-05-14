b1c2fa60a48ca585d4f8055c32436ae7ec2a27a5
gr0611 <rouxeling@hotmail.fr>
2018-05-14 15:28:39 +0200

    Ajout element dans BO

---
+++ SG-ParkEvent/BO/Evenement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class Evenement
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Lieu { get; set; }
+        public DateTime DateHeure { get; set; }
+        public DateTime Duree { get; set;  }
+        public string Theme { get; set; }
+        public string Descriptif { get; set; }
+        public virtual Images Image { get; set; }
+        public virtual Clients Client { get; set; }
+        public virtual Parkings Parkings { get; set; }
+
+    }
+}
