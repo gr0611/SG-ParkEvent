75a9c7f864a551b34cdc1ce486b5192930d1b5c5
RAOIDI SABRA <sabra.raoidi2016@campus-eni.fr>
2018-05-14 16:35:31 +0200

    ajout client bo

---
+++ SG-ParkEvent/BO/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    class Client
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        public DateTime DateDeNaissance { get; set; }
+        //J'ajoute 'virtual' si je fais appel à un objet externe
+       // public virtual Event Event { get; set; }
+        //public virtual Parking Parking { get; set; }
+    }
+}
