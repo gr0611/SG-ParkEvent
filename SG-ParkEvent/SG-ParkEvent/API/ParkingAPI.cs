1c374d389dc9d783b7419ddf23f2efda2d7929da
gr0611 <rouxeling@hotmail.fr>
2018-05-14 16:56:35 +0200

    ajout dossier api

---
+++ SG-ParkEvent/SG-ParkEvent/API/ParkingAPI.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace SG_ParkEvent.API
+{
+    public class ParkingAPI
+    {
+        //var client = new WebClient();
+        //var adresse = @"http://www.songsterr.com/a/ra/songs.json?pattern=Lofofora";
+        //var json = client.DownloadString(adresse);
+        //var obj = JsonConvert.DeserializeObject<List<RootObject>>(json);
+    }
+}
