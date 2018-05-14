93b7815fcc72e42ef81fc4e888a843cbae4428c6
gr0611 <rouxeling@hotmail.fr>
2018-05-14 14:09:04 +0200

    initial

---
+++ SG-ParkEvent/SG-ParkEvent/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SG_ParkEvent.MvcApplication" Language="C#" %>
---
+++ SG-ParkEvent/SG-ParkEvent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SG_ParkEvent.Startup))]
+namespace SG_ParkEvent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
