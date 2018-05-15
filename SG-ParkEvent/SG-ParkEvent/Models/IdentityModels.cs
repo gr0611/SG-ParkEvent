2d0cf126172ca1c2e13cee559238e70edc93fe0e
gr0611 <guillaume.rouxelin@gmail.com>
2018-05-15 11:20:57 +0200

    ajout migration

--- SG-ParkEvent/SG-ParkEvent/Models/IdentityModels.cs
+++ SG-ParkEvent/SG-ParkEvent/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace SG_ParkEvent.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("Context", throwIfV1Schema: false)
         {
         }
 
