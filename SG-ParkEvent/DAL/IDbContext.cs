c3266d8c021d3e7068b261381ada0dc1db730cc2
sraoidi <sabra.raoidi@yahoo.fr>
2018-05-15 09:35:23 +0200

    reation DAL

---
+++ SG-ParkEvent/DAL/IDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BO;
+using System.Data.Entity;
+
+
+namespace DAL
+{
+    class IDbContext
+    {
+        //DbSet<Client> Clients { get; set; }
+        //DbSet<Evenement> Competitors { get; set; }       
+    }
+}
