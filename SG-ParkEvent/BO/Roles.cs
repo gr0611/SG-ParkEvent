abf8439160c7f646fea70e59e829f3eddf10edb9
gr0611 <guillaume.rouxelin@gmail.com>
2018-05-15 10:24:30 +0200

    ajout role dans bo

---
+++ SG-ParkEvent/BO/Roles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BO
+{
+    public static class Roles 
+    {
+        public const string Administrateur = "Administrateur";
+        public const string Client = "Client";
+    }
+}
