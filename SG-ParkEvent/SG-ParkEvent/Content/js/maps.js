a169abc39825f9894ff86546265af96dbc22a28e
gr0611 <guillaume.rouxelin@gmail.com>
2018-05-17 16:47:42 +0200

    Intégration maps dans details-events$

---
+++ SG-ParkEvent/SG-ParkEvent/Content/js/maps.js
@@ -0,0 +1,148 @@
+function initialiser() {
+  var directionsDisplay;
+  var directionsService = new google.maps.DirectionsService();
+  var map = new google.maps.Map(document.getElementById('map'), {
+           mapTypeControl: false,
+           zoom: 12,
+           center: {lat: 48.1119800, lng: -1.6742900}
+         });
+
+
+ // Create an array of alphabetical characters used to label the markers.
+ var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+ // Add some markers to the map.
+ // Note: The code uses the JavaScript Array.prototype.map() method to
+ // create an array of markers based on a given "locations" array.
+ // The map() method here has nothing to do with the Google Maps API.
+
+const apiUrl = "http://data.citedia.com/r1/parks/?crs=EPSG:4326";
+
+function urlToJson() {
+  var HttpReq = new XMLHttpRequest();
+  HttpReq.open("GET",apiUrl,false);
+  HttpReq.send(null);
+  return JSON.parse(HttpReq.responseText);
+}
+
+
+function getParks(){
+  var maps = urlToJson();
+  var parks = maps.parks;
+  var features = maps.features.features;
+  var customParks = [];
+  // var lat =null;
+  // var lng = null;
+  parks.forEach(function(park){
+    var geo = null;
+      features.forEach(function(feature){
+        if(park.id == feature.id){
+          geo = feature.geometry.coordinates;
+        }
+      })
+      customParks.push({
+        name : park.parkInformation.name,
+        status : park.parkInformation.status,
+        max :park.parkInformation.max,
+        free: park.parkInformation.free,
+        id : park.id,
+        coordinatesLat : geo[1],
+        coordinatesLng : geo[0],
+      })
+  })
+  return customParks;
+}
+//var locations = getParks();
+var locations =[]
+items = getParks();
+items.forEach(function(item){
+   locations.push({
+     lat:item.coordinatesLat, lng: item.coordinatesLng
+   })
+ })
+console.log("locations",locations);
+ var markers = locations.map(function(location, i) {
+   return new google.maps.Marker({
+     position: location,
+     label: labels[i % labels.length]
+   });
+});
+
+  // Add a marker clusterer to manage the markers.
+  var markerCluster = new MarkerClusterer(map, markers,
+     {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+//new AutocompleteDirectionsHandler(map);
+
+var input = /** @type {!HTMLInputElement} */(
+           document.getElementById('pac-input'));
+
+       var types = document.getElementById('type-selector');
+       map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+       map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
+
+       var autocomplete = new google.maps.places.Autocomplete(input);
+       autocomplete.bindTo('bounds', map);
+
+       var infowindow = new google.maps.InfoWindow();
+       var marker = new google.maps.Marker({
+         map: map,
+         anchorPoint: new google.maps.Point(0, -29)
+       });
+
+       autocomplete.addListener('place_changed', function() {
+         infowindow.close();
+         marker.setVisible(false);
+         var place = autocomplete.getPlace();
+         if (!place.geometry) {
+           // User entered the name of a Place that was not suggested and
+           // pressed the Enter key, or the Place Details request failed.
+           window.alert("No details available for input: '" + place.name + "'");
+           return;
+         }
+
+         // If the place has a geometry, then present it on a map.
+         if (place.geometry.viewport) {
+           map.fitBounds(place.geometry.viewport);
+         } else {
+           map.setCenter(place.geometry.location);
+           map.setZoom(17);  // Why 17? Because it looks good.
+         }
+         marker.setIcon(/** @type {google.maps.Icon} */({
+           url: place.icon,
+           size: new google.maps.Size(71, 71),
+           origin: new google.maps.Point(0, 0),
+           anchor: new google.maps.Point(17, 34),
+           scaledSize: new google.maps.Size(35, 35)
+         }));
+         marker.setPosition(place.geometry.location);
+         marker.setVisible(true);
+
+         var address = '';
+         if (place.address_components) {
+           address = [
+             (place.address_components[0] && place.address_components[0].short_name || ''),
+             (place.address_components[1] && place.address_components[1].short_name || ''),
+             (place.address_components[2] && place.address_components[2].short_name || '')
+           ].join(' ');
+         }
+
+         infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+         infowindow.open(map, marker);
+       });
+
+       // Sets a listener on a radio button to change the filter type on Places
+       // Autocomplete.
+       function setupClickListener(id, types) {
+         var radioButton = document.getElementById(id);
+         radioButton.addEventListener('click', function() {
+           autocomplete.setTypes(types);
+         });
+       }
+
+       //var depart = null;
+       //var arrivee = null;
+       //var distance = google.maps.geometry.spherical.computeDistanceBetween( depart, arrivee )
+     }
+
+//APIKEY = AIzaSyAiUoBBAdg7beOlYsRuJsGO2vD0yZBa78Y
+/********************************************/
---
+++ SG-ParkEvent/SG-ParkEvent/Controllers/ImagesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BO;
+using SG_ParkEvent.Models;
+
+namespace SG_ParkEvent.Controllers
+{
+    public class ImagesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Images
+        public ActionResult Index()
+        {
+            return View(db.Image.ToList());
+        }
+
+        // GET: Images/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Image.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // GET: Images/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Images/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,NomImage,PathImage")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Image.Add(image);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(image);
+        }
+
+        // GET: Images/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Image.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,NomImage,PathImage")] Image image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(image).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(image);
+        }
+
+        // GET: Images/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Image image = db.Image.Find(id);
+            if (image == null)
+            {
+                return HttpNotFound();
+            }
+            return View(image);
+        }
+
+        // POST: Images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Image image = db.Image.Find(id);
+            db.Image.Remove(image);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
